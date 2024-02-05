8eea8b6cfaf612b4e7495ff72d36e78b0d1941d7
t-rindade <tr.ndade3@gmail.com>
2024-02-05 20:44:58 -0300

    entradona

--- src/main/java/com/senai/apisasc/models/FuncionarioModel.java
+++ src/main/java/com/senai/apisasc/models/FuncionarioModel.java
@@ -47,31 +47,31 @@ public class FuncionarioModel implements Serializable, UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return senha;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
